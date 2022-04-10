bf87e200813211b9cf634706fa50dabc88a4589a
王俊锋 <wang1137095129@foxmail.com>
2022-04-10 22:28:56 +0800

    first commit

---
+++ src/main/java/com/github/wang/sort/Util.java
@@ -0,0 +1,13 @@
+package com.github.wang.sort;
+
+public class Util {
+    public static <T extends Comparable<T>> boolean less(T[] arr, int lo, int hi) {
+        return arr[lo].compareTo(arr[hi]) < 0;
+    }
+
+    public static <T> void exch(T[] arr, int lo, int hi) {
+        T tmp = arr[lo];
+        arr[lo] = arr[hi];
+        arr[hi] = arr[lo];
+    }
+}
