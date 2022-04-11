99414391f514f3bef584ede40b92cbc844f40505
王俊锋 <wang1137095129@foxmail.com>
2022-04-11 23:22:03 +0800

    希尔排序递增序列优化，并提供了一个获得递增序列的实现

--- src/main/java/com/github/wang/sort/Shell.java
+++ src/main/java/com/github/wang/sort/Shell.java
@@ -2,7 +2,7 @@ package com.github.wang.sort;
 
 public class Shell {
     public static <T extends Comparable<T>> void sort(T[] arr, int step) {
-        for (int s = step; s > 0; s--) {
+        for (int s = step; s >= 1; s /= 3) {
             for (int i = s; i < arr.length; i++) {
                 for (int j = i; j >= s; j -= s) {
                     if (Util.less(arr, j, j - s)) {
@@ -13,5 +13,13 @@ public class Shell {
                 }
             }
         }
+    }
+
+    public static <T extends Comparable<T>> int getH(T[] arr) {
+        int h = 1;
+        while (h < arr.length / 3) {
+            h += 3 * h + 1;
+        }
+        return h;
     }
 }
